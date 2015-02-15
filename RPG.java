3a8ba8de2542e6452fcd83496c254690de137dcd
djeter277 <delorenzoj14@ecu.edu>
2015-02-14 20:53:24 -0500

    Update RPG.java

--- RPG.java
+++ RPG.java
@@ -41,51 +41,52 @@ public class Game {
         String weaponSelection = "";
     
             do{
-              System.out.println(">>SELECT A PRIMARY WEAPON<<");
-              for(i=0;i<weapons.length;i++){
-              System.out.println(i+1+" "+weapons[i]);
-              }
-              clearScreen();
-              weaponSelection = kbd.nextLine();
+                System.out.println(">>SELECT A PRIMARY WEAPON<<");
+                 for(i=0;i<weapons.length;i++){
+                    System.out.println(i+1+" "+weapons[i]);
+                }
+                clearScreen();
+                 weaponSelection = kbd.nextLine();
             
-              if(weaponSelection.equalsIgnoreCase(weapons[0]))
-                System.out.println(">>AHH, THE AXE THE WEAPON OF BRUT FORCE");
-              else if(weaponSelection.equalsIgnoreCase(weapons[1]))
-                System.out.println(">>THE BOW AND ARROW, A RATHER NOBLE CHOICE.");
-              else if(weaponSelection.equalsIgnoreCase(weapons[2]))
-                System.out.println(">>THE CHOICE WEAPON OF A LEADER.");
-              else if(weaponSelection.equalsIgnoreCase(weapons[3]))
-                System.out.println(">>A MAN OF GREAT STRENGTH MUST YOU BE TO WIELD THIS WEAPON");
-              else
-                System.out.println(">>INVALID SLECTION ");
+                if(weaponSelection.equalsIgnoreCase(weapons[0]))
+                    System.out.println(">>AHH, THE AXE THE WEAPON OF BRUT FORCE");
+                else if(weaponSelection.equalsIgnoreCase(weapons[1]))
+                    System.out.println(">>THE BOW AND ARROW, A RATHER NOBLE CHOICE.");
+                else if(weaponSelection.equalsIgnoreCase(weapons[2]))
+                    System.out.println(">>THE CHOICE WEAPON OF A LEADER.");
+                else if(weaponSelection.equalsIgnoreCase(weapons[3]))
+                    System.out.println(">>A MAN OF GREAT STRENGTH MUST YOU BE TO WIELD THIS WEAPON");
+                else
+                    System.out.println(">>INVALID SLECTION ");
             }while(!(weaponSelection.equalsIgnoreCase(weapons[0]))&&(!(weaponSelection.equalsIgnoreCase(weapons[1])))&&
                 (!(weaponSelection.equalsIgnoreCase(weapons[2]))) && (!(weaponSelection.equalsIgnoreCase(weapons[3]))));
+                
             clearScreen();
-            
             int j;
             String[] secWeapons = {"SICKLE","THROWING DAGGERS","SAWED OFF","SIX-SHOOTER"};
             String secWeaponSelection = "";
     
             do{
-              System.out.println(">>SELECT A SECONDARY WEAPON<<");
-              for(j=0;j<secWeapons.length;j++){
+                System.out.println(">>SELECT A SECONDARY WEAPON<<");
+                for(j=0;j<secWeapons.length;j++){
                 System.out.println(j+1+" "+secWeapons[j]);
-              }
-              clearScreen();
-              secWeaponSelection = kbd.nextLine();
+                }
+                clearScreen();
+                secWeaponSelection = kbd.nextLine();
             
-              if(secWeaponSelection.equalsIgnoreCase(secWeapons[0]))
-                System.out.println(">>PROVIDES A RATHER CLEAN CUT<<");
-              else if(secWeaponSelection.equalsIgnoreCase(weapons[1]))
-                System.out.println(">>A WEAPON OF GREAT PERCISION<<");
-              else if(secWeaponSelection.equalsIgnoreCase(secWeapons[2]))
-                System.out.println(">>QUITE THE GORY CHOICE<<");
-              else if(secWeaponSelection.equalsIgnoreCase(secWeapons[3]))
-                System.out.println(">>YOU MUST BE A GUN GUY..<<");
-              else
-              System.out.println(">>INVALID SLECTION ");
+                if(secWeaponSelection.equalsIgnoreCase(secWeapons[0]))
+                    System.out.println(">>PROVIDES A RATHER CLEAN CUT<<");
+                else if(secWeaponSelection.equalsIgnoreCase(weapons[1]))
+                    System.out.println(">>A WEAPON OF GREAT PERCISION<<");
+                else if(secWeaponSelection.equalsIgnoreCase(secWeapons[2]))
+                    System.out.println(">>QUITE THE GORY CHOICE<<");
+                else if(secWeaponSelection.equalsIgnoreCase(secWeapons[3]))
+                    System.out.println(">>YOU MUST BE A GUN GUY..<<");
+                else
+                    System.out.println(">>INVALID SLECTION ");
             }while(!(secWeaponSelection.equalsIgnoreCase(secWeapons[0]))&&(!(secWeaponSelection.equalsIgnoreCase(secWeapons[1]))&&
             (!(secWeaponSelection.equalsIgnoreCase(secWeapons[2]))) &&(!(secWeaponSelection.equalsIgnoreCase(secWeapons[3]))));
+            clearScreen();
     }
     public static void Continue()
     {
